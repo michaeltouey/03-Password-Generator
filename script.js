f1610d68567a1f8e18c9e386dc8dc421bd82c76e
Kobe <michaeltouey@Michaels-MacBook-Air.local>
2021-03-21 12:51:58 -0400

    password gen created

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 var generateBtn = document.querySelector("#generate");
 
 // arrays
-var lowerChar = ["m", "t", "q", "o", "n", "h", "d", "s", "k", "c"];
-var upperChar = ["E", "G", "A", "F", "X", "Z", "B", "W", "U", "H"];
-var ranNum = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specChar = ["/", "`", "~", "!", "@", "$", "^", "%"];
+var lowerChar = ["mtqonhdskc"];
+var upperChar = ["EGAFXZBWUH"];
+var ranNum = ["123456789"];
+var specChar = ["/`~!@$^%"];
 
 // get random index from array of lowerChar
 var indexL = Math.floor(Math.random() * lowerChar.length);
@@ -28,8 +28,77 @@ var computerChoice = specChar[indexS];
 // majority of code will be in this function
 // use confirm and prompts in here
 function generatePassword() {
+  var possibilites = "";
   var password = "";
 
+  //ask the user about length
+  var passLength = parseInt(prompt("What length?"));
+
+  //check if the length is an actual number
+  if (isNaN(passLength)) {
+    //send message
+    alert("hey this is not a number ...");
+
+    //exit
+    return;
+  }
+
+  //check if the length is greater than 8
+  if (passLength < 8) {
+    //send message
+    alert("hey this is too small ...");
+
+    //exit
+    return;
+  }
+  //check if the length is less than 128
+  if (passLength > 128) {
+    //send message
+    alert("hey this is too big ...");
+
+    //exit
+    return;
+  }
+
+  //collect the user info and build array of possibiliites
+
+  //lowerChar
+  var wantLowerChar = confirm("Do you want to include lower case characters?");
+  if (wantLowerChar) {
+    possibilites += lowerChar;
+  }
+
+  //upperChar
+  var wantUpperChar = confirm("Do you want to include upper case characters?");
+  if (wantUpperChar) {
+    possibilites += upperChar;
+  }
+
+  //ranNum
+  var wantRanNum = confirm("Do you want to include numbers?");
+  if (wantRanNum) {
+    possibilites += ranNum;
+  }
+
+  //specChar
+  var wantSpecChar = confirm("Do you want to include special characters?");
+  if (wantSpecChar) {
+    possibilites += specChar;
+  }
+
+  console.log("******possibilities:", possibilites);
+
+  //cycle through each array index to get random number
+  for (var i = 0; i < passLength; i++) {
+    //random number generator for num between 0 and passLength
+    var randomIndex = Math.floor(Math.random() * possibilites.length);
+    //based on index get random num
+    var randomChar = possibilites[randomIndex];
+    //add to password
+    password += randomChar;
+  };
+
+  console.log("******password:", password);
 
   return password;
 }
